291e4542c322f908ca0402026e46b10dca722fff
Eli Van fraeyenhove <eli@fenego.be>
2016-11-22 23:08:47 +0100

    changed port

--- src/main/java/eu/fancybrackets/template/verticle/MainVerticle.java
+++ src/main/java/eu/fancybrackets/template/verticle/MainVerticle.java
@@ -1,20 +1,29 @@
 package eu.fancybrackets.template.verticle;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import eu.fancybrackets.template.guice.ConfigModule;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.ResponseTimeHandler;
 
 public class MainVerticle extends AbstractVerticle {
 	Injector injector = null;
-
+	private final static Logger LOG = Logger.getLogger(MainVerticle.class.getName());
+	
 	public MainVerticle() {
 		super();
 	}
@@ -29,17 +38,23 @@ public class MainVerticle extends AbstractVerticle {
 
 		vertx.deployVerticle(injector.getInstance(RonSwansonVerticle.class));
 
-		vertx.createHttpServer().requestHandler(router::accept).listen(8081);
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 
-		printDBVersion(injector.getInstance(DSLContext.class));
+		printDBVersion(injector.getInstance(DataSource.class));
 	}
 
-	protected void printDBVersion(DSLContext dsl) {
+	protected void printDBVersion(DataSource ds) {
 		vertx.executeBlocking(future -> {
-			Record fetchOne = dsl.fetchOne("select version();");
-			future.complete(fetchOne.get(0));
+			try (Connection conn = ds.getConnection()) {
+				Record record = DSL.using(conn, SQLDialect.POSTGRES).fetchOne("select version();");
+				future.complete(record.get(0));
+			} catch (SQLException e) {
+				future.fail(e);
+				LOG.log(Level.SEVERE, "Unexpected exception", e);
+				e.printStackTrace();
+			}
 		}, res -> {
-			System.out.println("Connected to: " + res.result());
+			LOG.info(String.format("Connected to %s", res.result()));
 		});
 	}
 
